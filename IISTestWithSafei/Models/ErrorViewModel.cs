fbceccc5699ed66cf5ec858874762afc32333e05
Rowida Essam <rowidaessam21@gmail.com>
2022-07-12 15:57:03 +0200

    Initial

---
+++ IISTestWithSafei/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IISTestWithSafei.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ IISTestWithSafei/Models/TempStaff.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IISTestWithSafei.Models
+{
+    public partial class TempStaff
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Gender { get; set; }
+        public int Position { get; set; }
+    }
+}
